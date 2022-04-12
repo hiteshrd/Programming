91f07fd69c97efb36e685d8b24e75bc69cdbb485
Hitesh Gupta <37321492+hiteshrd@users.noreply.github.com>
2022-04-12 09:25:52 +0530

    Create 2022-04-12-289.GameofLife-solution.cs

---
+++ solutions/leetcode/2022-04-12-289.GameofLife-solution.cs
@@ -0,0 +1,48 @@
+public class Solution {
+    public void GameOfLife(int[][] board) {
+        int rows = board.Length;
+        int cols = board[0].Length;
+        int counter = 0;
+        SortedDictionary<int,int> myDic = new SortedDictionary<int,int>();
+        for(int i = 0; i<rows; i++)
+        {
+            for(int j = 0; j<cols; j++)
+            {
+                int nei = j +1 < cols ? board[i][j+1]: 0;
+                nei += j - 1 >= 0 ? board[i][j-1] : 0;
+                nei += i + 1 < rows ? board[i+1][j] : 0;
+                nei += i - 1 >= 0 ? board[i-1][j] : 0;
+                nei += j + 1 <cols && i+1 <rows ? board[i+1][j+1] : 0;
+                nei += j + 1 <cols && i-1 >= 0 ? board[i-1][j+1] : 0;
+                nei += j - 1 >= 0 && i+1 <rows ? board[i+1][j-1] : 0;
+                nei += j - 1 >= 0 && i-1 >= 0 ? board[i-1][j-1] : 0;
+                if(board[i][j] == 0 && nei == 3)
+                {
+                    myDic.Add(counter,1);
+                }
+                else if(board[i][j] == 1 && (nei == 2 || nei == 3))
+                {
+                    myDic.Add(counter,1);
+                }
+                else if(board[i][j] == 1 && (nei < 2 || nei > 3))
+                {
+                    myDic.Add(counter,0);
+                }
+                else
+                {
+                    myDic.Add(counter,board[i][j]);
+                }
+                counter++;
+            }
+        }
+        counter = 0;
+        for(int i = 0; i<rows; i++)
+        {
+            for(int j = 0; j<cols; j++)
+            {
+                board[i][j] = myDic[counter];
+                counter++;
+            }
+        }
+    }
+}
