77edf435b3136bc45c482f6ebf2042b8b3282639
Hitesh Gupta <37321492+hiteshrd@users.noreply.github.com>
2022-05-07 22:22:54 +0530

    Update 2022-05-06-1209.RemoveAllAdjacentDuplicatesinStringII-solution.cs

--- solutions/leetcode/2022/may-leetcoding-challenge/2022-05-06-1209.RemoveAllAdjacentDuplicatesinStringII-solution.cs
+++ solutions/leetcode/2022/may-leetcoding-challenge/2022-05-06-1209.RemoveAllAdjacentDuplicatesinStringII-solution.cs
@@ -1,1 +1,31 @@
 
+public class Solution {
+    public string RemoveDuplicates(string s, int k) {
+        Stack<(char val, int freq)> st = new Stack<(char, int)>();
+        int count = 0;
+        int times = 1;
+        foreach(var item in s)
+        {
+            if(st.Count > 0 && st.Peek().val == item )
+            {
+                var temp = st.Pop();
+                temp.freq++;
+                if(temp.freq != k)
+                {
+                    st.Push(temp);
+                }
+            }
+            else
+            {
+                st.Push((item,1));
+            }
+        }
+        StringBuilder result = new StringBuilder();
+		var resultList = st.ToArray().Reverse();
+		foreach ((char c, int freq) in resultList)            
+		{
+			result.Append(c, freq);
+		}
+		return result.ToString();
+    }
+}
